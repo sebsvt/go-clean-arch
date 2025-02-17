ee462f038f180568e771710772e4bbcf19b9469b
Saharat Vithchataya <vithchataya.muksarn@gmail.com>
2025-02-17 23:50:05 +0700

    upload adapter

--- domain/errors.go
+++ domain/errors.go
@@ -3,12 +3,9 @@ package domain
 import "errors"
 
 var (
-	// ErrInternalServerError will throw if any the Internal Server Error happen
-	ErrInternalServerError = errors.New("internal Server Error")
-	// ErrNotFound will throw if the requested item is not exists
-	ErrNotFound = errors.New("your requested Item is not found")
-	// ErrConflict will throw if the current action already exists
-	ErrConflict = errors.New("your Item already exist")
-	// ErrBadParamInput will throw if the given request-body or params is not valid
-	ErrBadParamInput = errors.New("given Param is not valid")
+	ErrRequireAtLeastTwoFile   = errors.New("at least two files are required to merge")
+	ErrCannotMerge             = errors.New("cannot merge files")
+	ErrCannotSplit             = errors.New("cannot split file")
+	ErrCannotCompress          = errors.New("cannot compress file")
+	ErrInvalidCompressionLevel = errors.New("invalid compression level")
 )
---
+++ internal/adapter/pdfcpu.go
@@ -0,0 +1,49 @@
+package adapter
+
+import (
+	"github.com/pdfcpu/pdfcpu/pkg/api"
+	"github.com/pdfcpu/pdfcpu/pkg/pdfcpu/model"
+)
+
+type PDFCPUAdapter struct {
+	config *model.Configuration
+}
+
+func NewPDFCPUAdapter() *PDFCPUAdapter {
+	cfg := model.NewDefaultConfiguration()
+	return &PDFCPUAdapter{
+		config: cfg,
+	}
+}
+
+func (p *PDFCPUAdapter) Merge(inputFiles []string, outputFile string) error {
+	return api.MergeCreateFile(inputFiles, outputFile, false, p.config)
+}
+
+func (p *PDFCPUAdapter) Split(inputFile string, outputDir string, pageNrs int) error {
+	err := api.SplitFile(inputFile, outputDir, pageNrs, p.config)
+	return err
+}
+
+func (p *PDFCPUAdapter) Compress(inputFile string, outputFile string, level string) error {
+	conf := model.NewDefaultConfiguration()
+	conf.Optimize = true
+	switch level {
+	case "extreme":
+		conf.OptimizeResourceDicts = true
+		conf.OptimizeDuplicateContentStreams = true
+	case "recommended":
+		conf.OptimizeResourceDicts = true
+		conf.OptimizeDuplicateContentStreams = false
+	case "low":
+		conf.OptimizeResourceDicts = false
+		conf.OptimizeDuplicateContentStreams = false
+	default:
+		// if level is not recognized, use recommended
+		conf.OptimizeResourceDicts = true
+		conf.OptimizeDuplicateContentStreams = false
+
+	}
+	err := api.OptimizeFile(inputFile, outputFile, conf)
+	return err
+}
